cda37d6a03cd445aa453f21f33b9e404a8d7e34d
Philippe Ta <philippe.ta@polymtl.ca>
2024-01-12 14:33:28 -0500

    button.cpp corrected to norms

--- tp1/button.cpp
+++ tp1/button.cpp
@@ -2,17 +2,27 @@
 #include <util/delay.h>
 
 int main(){
-    //mettre port A comme entree, les autres comme sortie
-    DDRA = 0xFF;
-    DDRB = 0xFF;
-    DDRC = 0xFF;
+    //mettre port D comme entree, les autres comme sorties
 
-    DDRD = 0x00; 
+    DDRA = 0x00;
+    DDRB = 0x00;
+    DDRC = 0x00;
+    DDRD = 0x00;
+
+    //DDRA = 0xFF;
+    //DDRB = 0xFF;
+    //DDRC = 0xFF;
+
+    DDRB |= (1 << PB0) | (1 << PB1);
+    DDRD &= ~(1 << PD2);
+
+    //DDRD = 0x00; 
 
     PORTB = 0x00;
 
     while(true){
-        if(PIND & 0x04){
+        //if(PIND & 0x04){
+        if(PIND & (1 << PD2)){
             _delay_ms(10);
             if(PIND & 0x04){
                 PORTB = 0x01;
@@ -21,6 +31,5 @@ int main(){
         else{
             PORTB = 0x00;
         }
-
     }
 }
